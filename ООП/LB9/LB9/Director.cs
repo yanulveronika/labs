3afd3e64e5dcae05f91f45b755466d3c1bc33c2d
Anton Savostin <asavostin23@gmail.com>
2019-10-30 00:26:15 +0300

    РћРћРџ Р»Р°Р±Р° 9

---
+++ ООП/LB9/LB9/Director.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LB9
+{
+    class Director
+    {
+        public void Up()
+        {
+            Console.WriteLine("Повышение!");
+        }
+        public void Down()
+        {
+            Console.WriteLine("Штраф!");
+        }
+    }
+    class Student
+    {
+        private string name;
+
+        public delegate void del();
+        public del _del;
+       
+        public void CallDelegate()
+        {
+
+            if (_del != null)
+            {
+                Console.WriteLine("Объект Student " + name + ' ');
+                _del();
+            }
+            else
+                Console.WriteLine("Объект Student " + name + " пустой делегат");
+            //_del?.Invoke();
+        }
+        public Student(string name) => this.name = name;
+
+    }
+    class Turner
+    {
+        public Turner(string name) => this.name = name;
+        private string name;
+        public delegate void del();
+        private del _deleg;
+        public event del deleg
+        {
+            add
+            {
+                _deleg += value;
+            }
+            remove
+            {
+                _deleg -= value;
+            }
+        }
+        public void CallDelegate()
+        {
+
+
+            if (_deleg != null)
+            {
+                Console.WriteLine("Объект Turner " + name + ' ');
+                _deleg.Invoke();
+            }
+            else
+                Console.WriteLine("Объект Turner " + name + " пустой делегат");
+            //_del?.Invoke();
+
+
+        }
+    }
+}
---
+++ ООП/LB9/LB9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LB9
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Director dir = new Director();
+            Student s1 = new Student("Vasya");
+            s1.CallDelegate();
+            s1._del = dir.Down;
+            s1._del += dir.Up;
+            s1._del();
+            s1.CallDelegate();
+            Turner t1 = new Turner("Petya");
+            t1.deleg += dir.Up;
+            t1.deleg += dir.Down;
+            t1.deleg += dir.Up;
+            t1.deleg += dir.Down;
+            t1.deleg -= dir.Down;
+            //t1.deleg();
+            t1.CallDelegate();
+            t1.deleg += delegate
+             {
+                 Console.WriteLine("Анонимная функция");
+             };
+            t1.deleg += () => Console.WriteLine("Лямбда-функция");
+            t1.CallDelegate();
+        }
+        
+    }
+}
